d7fac3a33ed027b7ce1f9e532b8d39f7dc22f189
Kelvin Feh <fehkelvink@gmail.com>
2025-11-27 15:11:14 +0100

    Working on vercel build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // Configuration options here
 };
 
 export default nextConfig;
